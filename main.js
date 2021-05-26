ccf4177eaa7c2b8250feaae7b6b128cd46bd92a5
burramukuneeraj <79148355+burramukuneeraj@users.noreply.github.com>
2021-05-26 11:03:36 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+var fisrt_name ="burramuku";
+console.log(fisrt_name)
+var last_name ="neeraj";
+console.log(last_name);
+var full_name= fisrt_name.concat(last_name);
+console.log(full_name);
+var a ="12";
+var b ="10";
+var multiply =a * b;
+console.log(multiply);
+var c ="20";
+var d ="2";
+var divide =c/d;
+console.log(divide)
